c7c9e6fdd2e2c34c813bba8ceb42ce34572a2a62
Brandon Berney <brandonberney@protonmail.com>
2015-08-20 13:49:59 -0400

    okay

---
+++ src/FromMeToYou.java
@@ -0,0 +1,15 @@
+/**
+ * Author: Brandon B.
+ * Date: 8-18-15
+ */
+ 
+public class FromMeToYou {
+    
+    public static void main(String[] args) {
+    	System.out.println("From: Bill Smith");
+    	System.out.println("Address: Dell Computer, Bldg 13");
+    	System.out.println("Date: April 12, 2005");
+    	System.out.println("\nTo: Jack Jones\n");
+    	System.out.println("Message: Help! I'm trapped inside a computer");
+    }
+}
